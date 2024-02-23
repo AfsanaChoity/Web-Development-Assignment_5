c1cfb90bc453a0ba005a39066efa67c287fa3bf6
choity <afsanamimi194@gmail.com>
2024-02-24 00:15:56 +0600

    first commit

---
+++ B9A5-smart-ticketing/script.js
@@ -0,0 +1,5 @@
+function goToParibahan() {
+    const buttonBuyTickets = document.getElementById("btn-buy-tickets");
+    const paribahanSection = document.getElementById("Shemoli-Paribahan");
+    paribahanSection.scrollIntoView();
+}
