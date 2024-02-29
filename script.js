d2388f5a3e07ac518819ed289eb96874a0660f43
choity <afsanamimi194@gmail.com>
2024-02-29 21:33:49 +0600

    recheck

--- script.js
+++ script.js
@@ -28,31 +28,31 @@ for (const btn of allBtn) {
     // console.log(btn);
     btn.addEventListener("click", function (e) {
 
-        
+
         const fare = 550;
         let button = btn.innerText;
         // console.log(typeof(fare));
         seatNumber++;
         totalSeat--;
-        if(totalSeat < 4){
+        if (totalSeat < 4) {
             alert("You can't select more than 4 seats")
             return;
-         }
+        }
         // console.log(totalSeat);
-        else{
+        else {
             btn.style.backgroundColor = "#1DD100";
             btn.style.color = "white";
             btn.disabled = true;
-             document.getElementById("total-seat").innerText = totalSeat;
-             document.getElementById("seat-number").innerText = seatNumber;
+            document.getElementById("total-seat").innerText = totalSeat;
+            document.getElementById("seat-number").innerText = seatNumber;
 
-             let para1 = document.createElement("p");
-             let para2 = document.createElement("p");
-             let para3 = document.createElement("p");
-             
-             para1.textContent = button;
-             para2.textContent = "Economy";
-             para3.textContent = "550";
+            let para1 = document.createElement("p");
+            let para2 = document.createElement("p");
+            let para3 = document.createElement("p");
+
+            para1.textContent = button;
+            para2.textContent = "Economy";
+            para3.textContent = "550";
             seatDetails.appendChild(para1);
             seatDetails.appendChild(para2);
             seatDetails.appendChild(para3);
@@ -61,8 +61,8 @@ for (const btn of allBtn) {
             document.getElementById("total-price").innerText = totalPrice;
             grandTotal = totalPrice;
             document.getElementById("grand-total").innerText = grandTotal;
-            
-             
+
+
         }
 
     });
@@ -70,25 +70,25 @@ for (const btn of allBtn) {
 
 function couponCode() {
     const couponContainer = document.getElementById("coupon-container");
-    let discount15 = (15*grandTotal)/100;
-    let discount20 = (20*grandTotal)/100;
+    let discount15 = (15 * grandTotal) / 100;
+    let discount20 = (20 * grandTotal) / 100;
     // console.log(discount15);
-    
+
     let input = document.getElementById("input-coupon").value.trim();
-    
-    if(input === "New15" ){
+
+    if (input === "NEW15") {
         couponContainer.classList.add('hidden');
-        
+
         grandTotal = grandTotal - discount15;
         document.getElementById("grand-total").innerText = grandTotal;
-        
+
     }
-    else if(input === "Couple 20"){
+    else if (input === "Couple 20") {
         couponContainer.classList.add('hidden');
         grandTotal = grandTotal - discount20;
         document.getElementById("grand-total").innerText = grandTotal;
     }
-    
+
 
 
 }
